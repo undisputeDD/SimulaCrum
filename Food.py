cfe069edb012f2a8e035998aee74bf558248cff5
okuzan <49767049+okuzan@users.noreply.github.com>
2020-06-23 00:28:13 +0300

    Update Food.py

--- Food.py
+++ Food.py
@@ -1,23 +1,28 @@
 import pygame
 
+
 class Food(pygame.sprite.Sprite):
     """
-    This class represents the ball.
+    This class represents the food.
     It derives from the "Sprite" class in Pygame.
     """
 
-    def __init__(self, screen):
+    def __init__(self, screen, style):
         """ Constructor. Pass in the color of the block,
         and its x and y position. """
 
         # Call the parent class (Sprite) constructor
         super().__init__()
-        image = pygame.image.load("../data/icons/gem.png").convert_alpha()
+        image = pygame.image.load(f'../data/icons/food/{style}.png').convert_alpha()
         image = pygame.transform.scale(image, (60, 60))
         # self.image = pygame.Surface([width, height])
         self.image = image
         self.screen = screen
+        self.x = 0
+        self.y = 0
         self.rect = self.image.get_rect()
 
     def draw(self, x, y):
         self.screen.blit(self.image, (x, y))
+        self.x = x
+        self.y = y
