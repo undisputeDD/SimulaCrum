6e229003410893f74bf0f51dc48b94041c0403ba
okuzan <49767049+okuzan@users.noreply.github.com>
2020-06-23 04:24:04 +0300

    Update graphico.py

--- graphico.py
+++ graphico.py
@@ -2,6 +2,7 @@ import matplotlib
 import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 from tkinter import *
 
 matplotlib.use('TkAgg')
@@ -34,15 +35,15 @@ def decode_template(filename):
     return data, catalogue
 
 
-class mclass:
+class Graphics:
 
     def __init__(self, root, catalogue):
         self.cat = catalogue
         self.window = root
         self.box = Entry(root)
         self.button = Button(root, text="check", command=self.plot)
-        self.box.pack()
-        self.button.pack()
+        self.box.grid(row=0, column=0)
+        self.button.grid(row=0, column=0)
 
     def process(self):
         for cat in self.cat:
@@ -70,9 +71,10 @@ class mclass:
                 survival_sum[idx] += float(survival) * val.item() / 100.
 
         print(span_sum)
-        fig = Figure(figsize=(6, 6))
-        a = fig.add_subplot(111)
 
+        fig = Figure(figsize=(5, 5))
+
+        a = fig.add_subplot(111)
         a.plot(speed_sum, c=np.random.rand(3, ), label='Speed')
         a.plot(survival_sum, c=np.random.rand(3, ), label='Survival')
         a.plot(span_sum, c=np.random.rand(3, ), label='Lifespan')
@@ -82,13 +84,62 @@ class mclass:
         a.set_title("Simulation Overview", fontsize=16)
         a.set_ylabel("Features", fontsize=14)
         a.set_xlabel("Time", fontsize=14)
-
         canvas = FigureCanvasTkAgg(fig, master=self.window)
-        canvas.get_tk_widget().pack()
+        canvas.get_tk_widget().grid(row=1, column=0)
+
+        # ____________________
+        fig2 = Figure(figsize=(5, 5))
+        b = fig2.add_subplot(111)
+        for key, value in data.items():
+            b.plot(value, c=np.random.rand(3, ), label=key)
+
+        fig2.legend(loc='upper right')
+        b.set_title("Population over time", fontsize=16)
+        b.set_ylabel("Population", fontsize=14)
+        b.set_xlabel("Time", fontsize=14)
+        canvas = FigureCanvasTkAgg(fig2, master=self.window)
+        canvas.get_tk_widget().grid(row=1, column=1)
+
+        # ____________________
+
+        labels = []
+        values = []
+        for key, value in data.items():
+            labels.append(key)
+            values.append(value[-1])
+
+        fig3 = plt.figure(figsize=(5, 5))
+        fig3.suptitle("Final Population", fontsize=18)
+        explode = list()
+        for _ in labels:
+            explode.append(0.03)
+        pie = plt.pie(values, labels=labels, explode=explode, autopct='%1.2f%%')
+        plt.legend(pie[0], labels, loc="upper right")
+        canvas = FigureCanvasTkAgg(fig3, master=self.window)
+        canvas.get_tk_widget().grid(row=0, column=1)
+
+        # ____________________
+        labels = []
+        values = []
+        for key, value in data.items():
+            labels.append(key)
+            values.append(value[1])
+
+        fig3 = plt.figure(figsize=(5, 5))
+        fig3.suptitle("Initial Population", fontsize=18)
+        explode = list()
+        for _ in labels:
+            explode.append(0.03)
+        pie = plt.pie(values, labels=labels, explode=explode, autopct='%1.2f%%')
+        plt.legend(pie[0], labels, loc="upper right")
+        canvas = FigureCanvasTkAgg(fig3, master=self.window)
+        canvas.get_tk_widget().grid(row=0, column=0)
+
+        # ______________________________
         canvas.draw()
 
 
 _, cat = decode_template("./data/scenarios/Fabula.crum")
 window = Tk()
-start = mclass(window, cat)
+start = Graphics(window, cat)
 window.mainloop()
